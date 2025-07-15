0eb4126c46cf759355162a1f47db6c3aa0d9b56c
Benjamin Marquez <bmarquez1997@gmail.com>
2025-07-15 14:54:51 -0400

    AnimStreamable support attempt 1

--- CUE4Parse-Conversion/Animations/UEFormat/UEAnim.cs
+++ CUE4Parse-Conversion/Animations/UEFormat/UEAnim.cs
@@ -108,4 +108,44 @@ public class UEAnim : UEFormatExport
         }
 
     }
+
+    public UEAnim(string name, UAnimStreamable animStreamable, ExporterOptions options) : base(name, options)
+    {
+        var framesPerSecond = animStreamable.NumFrames / animStreamable.SequenceLength;
+        using (var metaDataChunk = new FDataChunk("METADATA", 1))
+        {
+            metaDataChunk.Write(animStreamable.NumFrames);
+            metaDataChunk.Write(framesPerSecond);
+            
+            var referencePath = "empty";
+            metaDataChunk.WriteFString(referencePath);
+            
+            metaDataChunk.Write((byte) EAdditiveAnimationType.AAT_None); // EAdditiveAnimationType
+            metaDataChunk.Write((byte) EAdditiveBasePoseType.ABPT_None); // EAdditiveBasePoseType
+            metaDataChunk.Write(0);
+            
+            metaDataChunk.Serialize(Ar);
+        }
+
+        var floatCurves = animStreamable.RawCurveData.GetOrDefault<FFloatCurve[]>("FloatCurves", []);
+        if (floatCurves.Length > 0)
+        {
+            using var curveChunk = new FDataChunk("CURVES", floatCurves.Length);
+
+            foreach (var floatCurve in floatCurves)
+            {
+                // TODO serialize more data for better accuracy
+                curveChunk.WriteFString(floatCurve.CurveName.Text);
+                curveChunk.Write(floatCurve.FloatCurve.Keys.Length);
+                foreach (var floatCurveKey in floatCurve.FloatCurve.Keys)
+                {
+                    var key = new FFloatKey((int) (floatCurveKey.Time * framesPerSecond), floatCurveKey.Value);
+                    key.Serialize(curveChunk);
+                }
+            }
+
+            curveChunk.Serialize(Ar);
+        }
+
+    }
 }
